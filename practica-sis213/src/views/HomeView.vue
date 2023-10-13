b4afd536277d76f04861bb82b57412d8d4b98e3a
AndresAviles24 <130066347+AndresAviles24@users.noreply.github.com>
2023-10-13 00:55:27 +0000

    final

--- practica-sis213/src/views/HomeView.vue
+++ practica-sis213/src/views/HomeView.vue
@@ -1,9 +1,62 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h2>Lista de paises</h2>
+
+    <form @submit.prevent="saveTask()">
+        
+        <div class="form-group">
+          <label for="label">Etiqueta</label>
+          <div class="input-group">
+            <select class="form-control" id="label" v-model="form.labelId">
+              <option
+                v-for="country in countries"
+                :value="country.countryId"
+                :key="country.countryId"
+              >
+                {{ country.countryName }}
+              </option>
+            </select>
+          </div>
+        </div>
+
+        <br />
+        <div class="text-center">
+          <button id="submit-button" type="submit" class="btn btn-primary" style="background-color: hsl(120, 73%, 39%);">
+            Guardar
+          </button>
+        </div>
+      </form>
+
+
+  </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "homeView",
+  data: function(){
+    return {
+      form: {
+        countryId: "",
+        stateId: "",
+        cityId: "",
+      },
+      // Para listado
+      countries: [],
+      states: [],
+      cities: [],
+    };
+  },
+  mounted: function() {
+      //Llenado de las etiquetas
+      axios
+        .get("http://localhost:8080/api/v1/country")
+        .then((response) => {
+          this.countries = response.data.result;
+        });
+  }
+}
+
+</script>
